0cb4f7615b66f7e11054f24b8aba616e2c8a6db3
Kevin <kevin.castro.chavez0@gmail.com>
2023-02-03 16:05:20 -0700

    imported the mouse svg into the scrolldown component, and styled the components in home and the main sections for all devices

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import Social from './Social';
 import Data from './Data';
+import ScrollDown from './ScrollDown';
 
 function Home() {
   return (
@@ -14,6 +15,8 @@ function Home() {
 
           <Data />
         </div>
+
+        <ScrollDown />
       </div>
     </section>
   );
