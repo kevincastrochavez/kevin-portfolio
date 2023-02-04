91704b415d4cb9b51742eb8166bcb368d96d8db6
Kevin <kevin.castro.chavez0@gmail.com>
2023-02-04 16:37:19 -0700

    added styles for the qualification component for all devices

--- src/components/qualification.js
+++ src/components/qualification.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import SchoolIcon from '@mui/icons-material/School';
 import WorkIcon from '@mui/icons-material/Work';
 import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
 
 function Qualification() {
+  const [toggleState, setToggleState] = useState(1);
+
+  const toggleTab = (index) => {
+    setToggleState(index);
+  };
+
   return (
     <section className='qualification section'>
       <h2 className='section__title'>Qualification</h2>
@@ -11,24 +17,44 @@ function Qualification() {
 
       <div className='qualification__container container'>
         <div className='qualification__tabs'>
-          <div className='qualification__button qualification__active button--flex'>
+          <div
+            className={
+              toggleState === 1
+                ? 'qualification__button qualification__active button--flex'
+                : 'qualification__button button--flex'
+            }
+            onClick={() => toggleTab(1)}
+          >
             {' '}
             <SchoolIcon /> Education
           </div>
 
-          <div className='qualification__button button--flex'>
+          <div
+            className={
+              toggleState === 2
+                ? 'qualification__button qualification__active button--flex'
+                : 'qualification__button button--flex'
+            }
+            onClick={() => toggleTab(2)}
+          >
             {' '}
             <WorkIcon /> Experience
           </div>
         </div>
 
         <div className='qualification__sections'>
-          <div className='qualification__content'>
+          <div
+            className={
+              toggleState === 1
+                ? 'qualification__content qualification__content-active'
+                : 'qualification__content'
+            }
+          >
             <div className='qualification__data'>
               <div>
                 <h3 className='qualification__title'>Software Engineer</h3>
 
-                <span className='qualification__subtitle'>BYU-I</span>
+                <span className='qualification__subtitle'>BYU-I - USA</span>
 
                 <div className='qualification__calendar'>
                   <CalendarMonthIcon /> 2021 - Present
@@ -101,7 +127,13 @@ function Qualification() {
             </div>
           </div>
 
-          <div className='qualification__content'>
+          <div
+            className={
+              toggleState === 2
+                ? 'qualification__content qualification__content-active'
+                : 'qualification__content'
+            }
+          >
             <div className='qualification__data'>
               <div>
                 <h3 className='qualification__title'>Software Engineer</h3>
@@ -155,26 +187,6 @@ function Qualification() {
                 <span className='qualification__rounder'></span>
 
                 <span className='qualification__line'></span>
-              </div>
-            </div>
-
-            <div className='qualification__data'>
-              <div></div>
-
-              <div>
-                <span className='qualification__rounder'></span>
-
-                <span className='qualification__line'></span>
-              </div>
-
-              <div>
-                <h3 className='qualification__title'>Family Search</h3>
-
-                <span className='qualification__subtitle'>BYU-I</span>
-
-                <div className='qualification__calendar'>
-                  <CalendarMonthIcon /> 2021 - Present
-                </div>
               </div>
             </div>
           </div>
