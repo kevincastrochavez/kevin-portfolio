0cd2b8ad311e5eb8490f42dee7926b591fbcd84d
Kevin Castro <cas20057@byui.edu>
2023-12-23 14:56:06 -0700

    deleted services

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import Header from './components/Header';
 import About from './components/About';
 import Home from './components/Home';
 import Skills from './components/skills/Skills';
-import Services from './components/Services';
 import Experience from './components/Experience';
 import Contact from './components/Contact';
 
@@ -15,7 +14,6 @@ function App() {
         <Home />
         <About />
         <Skills />
-        {/* <Services /> */}
         <Experience />
         <Contact />
       </main>
