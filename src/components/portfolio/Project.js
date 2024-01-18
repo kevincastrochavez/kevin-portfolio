6607360dd3932a23cdc9da10d54e6525d4a88c78
Kevin Castro <cas20057@byui.edu>
2024-01-17 22:28:51 -0700

    gave cards more space customized them and improved layout

--- src/components/portfolio/Project.js
+++ src/components/portfolio/Project.js
@@ -1,15 +1,36 @@
 import React from 'react';
 
-import ArrowRightAltIcon from '@mui/icons-material/ArrowRightAlt';
+import { Tooltip } from '@mui/material';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import VisibilityIcon from '@mui/icons-material/Visibility';
 
 function Project({ item }) {
   return (
     <div className='project__card' key={item.id}>
       <img src={item.image} alt='' className='project__img' />
       <h3 className='project__title'>{item.title}</h3>
-      <a href='#' className='project__button'>
-        Demo <ArrowRightAltIcon />
-      </a>
+      <div className='project__actions'>
+        <Tooltip title='GitHub repository'>
+          <a
+            className='project__link'
+            target='_blank'
+            href={item?.githubLink}
+            rel='noreferrer'
+          >
+            <GitHubIcon />
+          </a>
+        </Tooltip>
+        <Tooltip title='View Live Site'>
+          <a
+            className='project__link'
+            target='_blank'
+            href={item?.liveSiteLink}
+            rel='noreferrer'
+          >
+            <VisibilityIcon />
+          </a>
+        </Tooltip>
+      </div>
     </div>
   );
 }
