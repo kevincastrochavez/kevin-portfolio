bc0c4b1f06d97bca161ce1679179a805fd3e3c6a
Kevin Castro <cas20057@byui.edu>
2024-01-22 16:08:37 -0700

    modified menu to be on the right side instead of at the top

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -13,7 +13,6 @@ import { useHeightScrolled } from '../../utils';
 
 function Header() {
   // TODO: Modify header to a side drawer
-  // TODO: Select color for wayfinder
   const [toggleMenu, setToggleMenu] = useState(false);
   const { heightScrolled } = useHeightScrolled();
   const minHeightToShowNavShadow = 30;
