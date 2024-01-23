6c3e56badd8888fdf1027aea50bcc69cfa55c0ff
Kevin Castro <cas20057@byui.edu>
2024-01-22 18:21:17 -0700

    updated data with more projects

--- src/components/portfolio/data.js
+++ src/components/portfolio/data.js
@@ -3,6 +3,9 @@ import covidTrackerImg from '../../assets/covidTracker.png';
 import mentImg from '../../assets/ment.png';
 import shuttleTrackerImg from '../../assets/shuttleTracker.png';
 import weddingImg from '../../assets/wedding.png';
+import portalSceneImg from '../../assets/portalScene.png';
+import hauntedHouseImg from '../../assets/hauntedHouse.png';
+import text3dImg from '../../assets/text3d.png';
 
 const imageExample = 'https://dummyimage.com/300';
 // const carstroImg =
@@ -47,6 +50,30 @@ export const projects = [
     category: 'web',
     githubLink: 'https://github.com/kevincastrochavez/Covid-19-Tracker',
     liveSiteLink: 'https://covid-19-tracker-6990c.web.app/',
+  },
+  {
+    id: 6,
+    image: portalSceneImg,
+    title: 'Portal Scene',
+    category: '3D',
+    githubLink: 'https://github.com/kevincastrochavez/threejs-importing-scene',
+    liveSiteLink: 'https://webgl-portal-scene-taupe.vercel.app/',
+  },
+  {
+    id: 7,
+    image: hauntedHouseImg,
+    title: 'Haunted House',
+    category: '3D',
+    githubLink: 'https://github.com/kevincastrochavez/threejs-haunted-house',
+    liveSiteLink: 'https://webgl-haunted-house-kevin.vercel.app/',
+  },
+  {
+    id: 8,
+    image: text3dImg,
+    title: '3D Text',
+    category: '3D',
+    githubLink: 'https://github.com/kevincastrochavez/threejs-3d-text',
+    liveSiteLink: 'https://webgl-3d-text-roan-omega.vercel.app/',
   },
 ];
 
