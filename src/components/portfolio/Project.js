e64817e6421e3456ab7bcef120e252dfc17a92af
Kevin Castro <cas20057@byui.edu>
2024-04-06 02:20:36 -0600

    modified the swiper and some styles

--- src/components/portfolio/Project.js
+++ src/components/portfolio/Project.js
@@ -2,44 +2,50 @@ import React from 'react';
 
 import { Tooltip } from '@mui/material';
 import GitHubIcon from '@mui/icons-material/GitHub';
+import InfoIcon from '@mui/icons-material/Info';
 import VisibilityIcon from '@mui/icons-material/Visibility';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { EffectFade, Navigation, Pagination } from 'swiper/modules';
+import { EffectFade, Pagination } from 'swiper/modules';
 
 import 'swiper/css';
 import 'swiper/css/effect-fade';
-import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 
 function Project({ item }) {
   return (
     <div className='project__card' key={item.id}>
       <Swiper
+        pagination={true}
         className='project__img'
         effect={'fade'}
         loop={true}
-        navigation={true}
-        modules={[EffectFade, Navigation]}
+        modules={[EffectFade, Pagination]}
       >
         {item.images?.map((image) => (
-          <SwiperSlide>
+          <SwiperSlide key={image}>
             <img src={image} alt={image} key={image} />
-            {/* <div className='project__overlay'></div> */}
           </SwiperSlide>
         ))}
       </Swiper>
       <h3 className='project__title'>{item.title}</h3>
       <div className='project__actions'>
-        <Tooltip title='GitHub repository'>
-          <a
-            className='project__link'
-            target='_blank'
-            href={item?.githubLink}
-            rel='noreferrer'
-          >
-            <GitHubIcon />
-          </a>
-        </Tooltip>
+        {item?.githubLink && (
+          <Tooltip title='GitHub repository'>
+            <a
+              className='project__link'
+              target='_blank'
+              href={item.githubLink}
+              rel='noreferrer'
+            >
+              <GitHubIcon />
+            </a>
+          </Tooltip>
+        )}
+        {!item?.isOwnApp && (
+          <Tooltip title={item.infoMessage}>
+            <InfoIcon />
+          </Tooltip>
+        )}
 
         <Tooltip title='View Live Site'>
           <a
