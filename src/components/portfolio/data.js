ceb932541abef885f772201cbcc8cc45ce1fe1b7
Kevin Castro <cas20057@byui.edu>
2024-04-06 02:20:15 -0600

    imported new projects into the data json

--- src/components/portfolio/data.js
+++ src/components/portfolio/data.js
@@ -1,3 +1,8 @@
+import familySearchOne from '../../assets/familySearchOne.png';
+import familySearchTwo from '../../assets/familySearchTwo.png';
+import familySearchThree from '../../assets/familySearchThree.png';
+import familySearchFour from '../../assets/familySearchFour.png';
+
 import carstroOne from '../../assets/carstroOne.png';
 import carstroTwo from '../../assets/carstroTwo.png';
 import carstroThree from '../../assets/carstroThree.png';
@@ -7,6 +12,12 @@ import carstroFive from '../../assets/carstroFive.png';
 import shuttleTrackerOne from '../../assets/shuttleTrackerOne.png';
 import shuttleTrackerTwo from '../../assets/shuttleTrackerTwo.png';
 import shuttleTrackerThree from '../../assets/shuttleTrackerThree.png';
+
+import todoOne from '../../assets/todoOne.png';
+import todoTwo from '../../assets/todoTwo.png';
+import todoThree from '../../assets/todoThree.png';
+import todoFour from '../../assets/todoFour.png';
+import todoFive from '../../assets/todoFive.png';
 
 import weddingOne from '../../assets/weddingOne.png';
 import weddingTwo from '../../assets/weddingTwo.png';
@@ -38,67 +49,99 @@ import covidTrackerImg from '../../assets/covidTracker.png';
 export const projects = [
   {
     id: 1,
+    images: [
+      familySearchOne,
+      familySearchTwo,
+      familySearchThree,
+      familySearchFour,
+    ],
+    title: 'FamilySearch',
+    category: 'web',
+    liveSiteLink: 'https://www.familysearch.org/en/',
+    isOwnApp: false,
+    infoMessage:
+      'As an intern, I worked on the search-related pages, dealing with complex logic and performance',
+  },
+  {
+    id: 2,
     images: [carstroOne, carstroTwo, carstroThree, carstroFour, carstroFive],
     title: 'Carstro Cars',
     category: 'web',
     githubLink: 'https://github.com/kevincastrochavez/carstro',
     liveSiteLink: 'https://carstro-15495.web.app/',
+    isOwnApp: true,
   },
   {
-    id: 2,
+    id: 3,
     images: [shuttleTrackerOne, shuttleTrackerTwo, shuttleTrackerThree],
     title: 'Shuttle Tracker',
     category: 'web',
     githubLink: 'https://github.com/kevincastrochavez/shuttletracker',
     liveSiteLink: 'https://shuttle-tracker-3997d.web.app/',
+    isOwnApp: true,
   },
   {
-    id: 3,
+    id: 4,
     images: [weddingOne, weddingTwo, weddingThree, weddingFour, weddingFive],
     title: 'Own Wedding Site',
     category: 'web',
     githubLink: 'https://github.com/kevincastrochavez/wedding',
     liveSiteLink: 'https://wedding-8b90a.web.app/',
+    isOwnApp: true,
   },
   {
-    id: 4,
+    id: 5,
+    images: [todoOne, todoTwo, todoThree, todoFour, todoFive],
+    title: 'Todo App',
+    category: 'web',
+    githubLink: 'https://github.com/kevincastrochavez/react-todo',
+    liveSiteLink: 'https://react-todo-weld-eight.vercel.app/',
+    isOwnApp: true,
+  },
+  {
+    id: 6,
     images: [mentOne, mentTwo, mentThree, mentFour, mentFive],
     title: 'MENT Films',
     category: 'web',
     githubLink: 'https://github.com/kevincastrochavez/ment',
     liveSiteLink: 'https://ment-films.web.app/',
+    isOwnApp: true,
   },
   {
-    id: 5,
+    id: 7,
     images: [covidTrackerImg],
     title: 'Covid Tracker',
     category: 'web',
     githubLink: 'https://github.com/kevincastrochavez/Covid-19-Tracker',
     liveSiteLink: 'https://covid-19-tracker-6990c.web.app/',
+    isOwnApp: true,
   },
   {
-    id: 6,
+    id: 8,
     images: [portalSceneOne, portalSceneTwo, portalSceneThree, portalSceneFour],
     title: '3D Portal Scene',
     category: '3D',
     githubLink: 'https://github.com/kevincastrochavez/threejs-importing-scene',
     liveSiteLink: 'https://webgl-portal-scene-taupe.vercel.app/',
+    isOwnApp: true,
   },
   {
-    id: 7,
+    id: 9,
     images: [hauntedHouseOne, hauntedHouseTwo, hauntedHouseThree],
     title: '3D Haunted House',
     category: '3D',
     githubLink: 'https://github.com/kevincastrochavez/threejs-haunted-house',
     liveSiteLink: 'https://webgl-haunted-house-kevin.vercel.app/',
+    isOwnApp: true,
   },
   {
-    id: 8,
+    id: 10,
     images: [text3dOne, text3dTwo, text3dThree],
     title: '3D Text',
     category: '3D',
     githubLink: 'https://github.com/kevincastrochavez/threejs-3d-text',
     liveSiteLink: 'https://webgl-3d-text-roan-omega.vercel.app/',
+    isOwnApp: true,
   },
 ];
 
